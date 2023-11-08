d624d272dcc34abba756e5571ceabdc1fe68cf8f
anas <anwar00anas@gmail.com>
2023-11-08 03:37:21 -0800

    py5

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program that:
-# demonstrates how to print x elements of a list.
-# demonstrates how to use a try ... except for exception handling
-#
-# (C) 2022 Igbinijesu Samuel, Lagos, Nigeria
-# email igbinijesusamuel@gmail.com
-# -----------------------------------------------------------
-
-
 def safe_print_list(my_list=[], x=0):
-    num_of_elements = 0
-    for index in range(x):
+    count = 0
+    for i in range(x):
         try:
-            print(my_list[index], end="")
-            num_of_elements += 1
+            print("{}".format(my_list[i]), end="")
+            count = count + 1
         except IndexError:
-            break
-
-    print()
-    return num_of_elements
+            continue
+    print("")
+    return count
